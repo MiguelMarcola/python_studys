ccb70bcc9c1f94a3e9c9b47be756b555fb13b5ed
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-23 18:51:27 -0300

    implement abstract method

---
+++ collections/array.py
@@ -0,0 +1,11 @@
+import array as arr
+import numpy as np
+
+array = arr.array('d', [1, 2, 3, 5])
+print(array)
+
+numeros = np.array([1., 3.5])
+print(numeros)
+
+numeros = numeros + 3
+print(numeros)
--- collections/heranca-polimorfismo.py
+++ collections/heranca-polimorfismo.py
@@ -1,4 +1,7 @@
-class Conta():
+from abc import ABCMeta, abstractclassmethod
+
+
+class Conta(metaclass=ABCMeta):
     def __init__(self, codigo):
         self._codigo = codigo
         self._saldo = 0
@@ -8,6 +11,10 @@ class Conta():
 
     def __str__(self) -> str:
         return f"[>>Código {self._codigo} Saldo {self._saldo}<<]"
+
+    @abstractclassmethod
+    def passa_o_mes(self):
+        pass
 
 
 class ContaCorrente(Conta):
@@ -20,6 +27,10 @@ class ContaPoupanca(Conta):
     def passa_o_mes(self):
         self._saldo *= 1.01
         self._saldo -= 3
+
+
+class ContaInvestimento(Conta):
+    pass
 
 
 conta16 = ContaCorrente(16)
@@ -35,3 +46,7 @@ contas = [conta16, conta17]
 for conta in contas:
     conta.passa_o_mes()
     print(conta)
+
+# ContaInvestimento(121323)
+# Error Can't instantiate abstract class ContaInvestimento with abstract method passa_o_mes
+#
