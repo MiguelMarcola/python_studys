91c445692842bc3ee91e011e606bbf03df40bb18
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-23 20:04:22 -0300

    evolve compares

--- collections/sequence.py
+++ collections/sequence.py
@@ -1,3 +1,6 @@
+from operator import attrgetter
+from equal import ContaSalario
+
 idades = [15, 87, 65, 56, 42, 30, 10, 87]
 
 for i in range(len(idades)):
@@ -30,3 +33,30 @@ print(sorted(idades, reverse=True))
 
 idades.sort()
 print(idades)
+
+nomes = ["Guilherme", "Daniela", "Paulo"]
+print(sorted(nomes))
+
+
+conta_gui = ContaSalario(1)
+conta_gui.deposito(1000)
+
+conta_dani = ContaSalario(2)
+conta_dani.deposito(100)
+
+conta_paulo = ContaSalario(3)
+conta_paulo.deposito(300)
+
+contas = [conta_gui, conta_dani, conta_paulo]
+
+
+def extrai_saldo(conta):
+    return conta.saldo
+
+
+for conta in sorted(contas, key=extrai_saldo):
+    print(conta)
+
+
+for conta in sorted(contas, key=attrgetter("saldo")):
+    print(conta)
