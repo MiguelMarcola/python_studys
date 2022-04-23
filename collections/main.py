08e0d3ce69549db52142dd6dff2af4b7c3a15e3f
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-23 16:00:15 -0300

    evolue in collections

--- collections/main.py
+++ collections/main.py
@@ -18,6 +18,42 @@ idades.remove(31)
 
 print(idades)
 
-idades.clear()
+# idades.clear()
 
 print(idades)
+
+28 in idades
+
+if 18 in idades:
+    idades.remove(18)
+
+
+idades.insert(0, 20)
+print(idades)
+
+
+idades.extend([19, 14])
+print(idades)
+
+
+idades_no_ano_que_vem = []
+
+for idade in idades:
+    idades_no_ano_que_vem.append(idade + 1)
+
+print(idades_no_ano_que_vem)
+
+
+idades_no_ano_que_vem_2 = [(idade+1) for idade in idades]
+print(idades_no_ano_que_vem_2)
+
+
+teste = [idade for idade in idades if idade > 21]
+print(teste)
+
+
+def proximo_ano(idade):
+    return idade + 1
+
+
+print([proximo_ano(idade) for idade in idades if idade > 21])
