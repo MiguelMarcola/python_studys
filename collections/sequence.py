904dbb67fc51a4ca6be7e42c656814518f7df292
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-23 19:48:56 -0300

    using enumerate

---
+++ collections/sequence.py
@@ -0,0 +1,24 @@
+idades = [15, 87, 65, 56, 42, 30, 10, 87]
+
+for i in range(len(idades)):
+    print(i, idades[i])
+
+print(list(range(len(idades))))
+print(list(enumerate(idades)))
+
+for indice, idade in enumerate(idades):
+    print(indice, "x", idade)
+
+
+usuarios = [
+    ("Guilherme", 37, 1981),
+    ("Daniela", 24, 1998),
+    ("Paulo", 39, 1979)
+]
+
+for nome, idade, nacsimento in usuarios:
+    print(nome)
+
+
+for nome, _, _ in usuarios:
+    print(nome)
