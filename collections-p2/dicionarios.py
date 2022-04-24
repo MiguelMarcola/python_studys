6b1311e3d9ed0cd7ccb244537f12917f01d97bd2
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-24 17:06:35 -0300

    contunie collections

--- collections-p2/dicionarios.py
+++ collections-p2/dicionarios.py
@@ -1,3 +1,6 @@
+from cgi import test
+
+
 aparicoes = {
     "Guilherme": 1,
     "cachorro": 2,
@@ -12,3 +15,39 @@ print(aparicoes["Guilherme"])
 
 print(aparicoes.get("xpto", 0))
 print(aparicoes.get("cachorro", 0))
+
+
+# aparicoes = dict(Guilherme = 2, cachorro = 1)
+
+aparicoes["Carlos"] = 1
+print(aparicoes)
+aparicoes["Carlos"] = 2
+print(aparicoes)
+del aparicoes["Carlos"]
+print(aparicoes)
+
+print("cachorro" in aparicoes)
+print("Carlos" in aparicoes)
+
+for elemento in aparicoes:
+    print(elemento)
+
+
+for elemento in aparicoes.values():
+    print(elemento)
+
+
+for elemento in aparicoes.keys():
+    valor = aparicoes[elemento]
+    print(elemento, valor)
+
+for elemento in aparicoes.items():
+    print(elemento)
+
+
+for chave, valor in aparicoes.items():
+    print(chave, "=", valor)
+
+
+teste = [f"palavra {chave}" for chave in aparicoes.keys()]
+print(teste)
