595ff3b806f0ca7e2cbcbf8cf3b7dd4b69375ff2
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-25 23:32:37 -0300

    refectoring

--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -1,10 +1,6 @@
-from cpf_cnpj import Document
+import re
 
-cpf = "16905495727"
-cnpj = "24632774000101"
-
-object_cpf = Document.create_doc(cpf)
-object_cnpj = Document.create_doc(cnpj)
-
-print(object_cpf)
-print(object_cnpj)
+padrao = "\w@\w.\w"
+texto = "grewgf ateste@email.com dasdasasd@dasd.1dasd dasdw dadwf"
+resposta = re.search(padrao, texto)
+print(resposta.group)
