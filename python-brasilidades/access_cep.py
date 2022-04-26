cf25edc204176407e01f71a71ada88e4d56f5d9b
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-26 18:54:11 -0300

    add api of CEP

--- python-brasilidades/access_cep.py
+++ python-brasilidades/access_cep.py
@@ -1,15 +1,18 @@
-from email.policy import default
 import re
+import requests
 
 
 class SearchAddress:
     def __init__(self, cep):
+        cep = str(cep)
         if (self.validator_cep(cep)):
             self.cep = cep
         else:
             raise ValueError("Invalid CEP!")
 
     def validator_cep(self, cep):
+        if len(cep) != 8:
+            return False
         default = "([0-9]{5})([0-9]{3})"
         response = re.search(default, cep)
         return response
@@ -19,6 +22,11 @@ class SearchAddress:
         response = re.search(default, self.cep)
         mask_cpf = f"{response.group(1)}-{response.group(2)}"
         return mask_cpf
+
+    def request_api(self):
+        request = requests.get(f"https://viacep.com.br/ws/{self.cep}/json")
+        response = request.text
+        print(response)
 
     def __str__(self) -> str:
         return f"{self.mask_cpf()}"
--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -1,5 +1,7 @@
+from audioop import add
 from access_cep import SearchAddress
 
-address = SearchAddress("123123122")
+address = SearchAddress("27197000")
 
 print(address)
+address.request_api()
