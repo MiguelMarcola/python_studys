3be9cc6a62509dd486acf4e2ed0c59e42dcb033c
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-25 23:53:07 -0300

    create number mask

--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -3,3 +3,5 @@ from telephones import Tel
 telefone = "24999020698"
 
 telefone_objeto = Tel(telefone)
+
+print(telefone_objeto)
--- python-brasilidades/telephones.py
+++ python-brasilidades/telephones.py
@@ -9,9 +9,21 @@ class Tel:
             raise ValueError("Invalid Number!")
 
     def validator_tel(self, number):
-        default = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+        default = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
         response = re.search(default, number)
         if response:
             return True
         else:
             return False
+
+    def mask_number(self):
+        default = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        response = re.search(default, self.number)
+        if response.group(1):
+            mask_number = f"+{response.group(1)}({response.group(2)}){response.group(3)}-{response.group(4)}"
+        else:
+            mask_number = f"+55({response.group(2)}){response.group(3)}-{response.group(4)}"
+        return mask_number
+
+    def __str__(self):
+        return self.mask_number()
