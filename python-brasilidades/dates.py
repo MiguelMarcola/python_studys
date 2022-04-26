a4b6087ec1be86d8d803f42b668da2aad3543c4c
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-26 00:05:25 -0300

    create day of week method

--- python-brasilidades/dates.py
+++ python-brasilidades/dates.py
@@ -1,3 +1,4 @@
+from calendar import month
 from datetime import datetime
 
 
@@ -5,5 +6,17 @@ class Dates:
     def __init__(self):
         self.created_at = datetime.today()
 
+    def month_created(self):
+        months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                  "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+        month_created = months[self.created_at.month - 1]
+        return month_created
+
+    def day_week(self):
+        days = ["Segunda-feira", "Terça-feira", "Quarta-feira",
+                "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"]
+        day_of_week = days[self.created_at.weekday()]
+        return day_of_week
+
     def __str__(self):
-        return f"{self.created_at}"
+        return f"{self.day_week()} {self.month_created()}"
